cae524609ea1579d7b22875a8f5ad106c38ba0e3
Abdulrhman Muhammad Abdulbadiee <eng.abdulrhman0@gmail.com>
2023-02-15 11:31:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ app.use(cors({ origin: '*' }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/hello', (req, res) => {
-    res.send('Hi!');
+app.post('/validateSession', async (req, res) => {
+    const { appleUrl } = req.body;
+    console.log(appleUrl);
+    
+    res.send('ok');
 });
-
 app.listen(port, () => {
     console.log('💪 Server running on ➡️ ', `http://localhost:${port}`);
 });
