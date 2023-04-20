da3cfdea452cf2df5c682ef1b16477c48da5e17e
Abdulrhman Muhammad Abdulbadiee <eng.abdulrhman0@gmail.com>
2023-04-20 18:15:33 +0200

    Update app.js

--- applev/app.js
+++ applev/app.js
@@ -163,7 +163,8 @@ var applePayController = (function (uiController) {
     // Apple Pay Session from your Back-End
     appleSession.onvalidatemerchant = function (event) {
       _validateApplePaySession(event.validationURL, function (merchantSession) {
-        appleSession.completeMerchantValidation(merchantSession)
+       // appleSession.completeMerchantValidation(merchantSession)
+          console.log(34);
       })
     }
 
