1dc7865b6a045305ee150079a9d4cf568db51ee5
Abdulrhman Muhammad Abdulbadiee <eng.abdulrhman0@gmail.com>
2023-09-07 21:05:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.get('/.well-known/apple-developer-merchantid-domain-association.txt', functi
 });
 
 
-app.get('/validateSession', async (req, res) => {
+app.post('/validateSession', async (req, res) => {
   
   
   
