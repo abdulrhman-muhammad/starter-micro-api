a6a346121db86b2cd1ca993ffbaf890a8427e56f
Abdulrhman Muhammad Abdulbadiee <eng.abdulrhman0@gmail.com>
2023-09-10 03:57:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -112,36 +112,51 @@ app.post('/pay', async (req, res) => {
 
 
 
-var request = require("request");
+// var request = require("request");
 
-var pOptions = { method: 'POST',
-  url: 'https://api.tap.company/v2/tokens',
-  headers: 
-   { 'content-type': 'application/json',
-     authorization: 'Bearer pk_live_JC2fuQ9SNysMh4ant8ebrXUd' },
-  body: 
-   { type: 'applepay',
-     token_data: 
-      { data: req.body.token.paymentData.data,
+// var pOptions = { method: 'POST',
+//   url: 'https://api.tap.company/v2/tokens',
+//   headers: 
+//    { 'content-type': 'application/json',
+//      authorization: 'Bearer pk_live_JC2fuQ9SNysMh4ant8ebrXUd' },
+//   body: 
+//    { type: 'applepay',
+//      token_data: 
+//       { data: req.body.token.paymentData.data,
+//         header: 
+//          { ephemeralPublicKey: req.body.token.paymentData.header.ephemeralPublicKey,
+//            publicKeyHash: req.body.token.paymentData.header.publicKeyHash,
+//            transactionId: req.body.token.paymentData.header.transactionId },
+//         signature: req.body.token.paymentData.signature,
+//          version: req.body.token.paymentData.version },
+//      client_ip: '192.168.1.20' },
+//   json: true };
+
+// request(pOptions, function (error, response, body) {
+//   console.log('77777787776');
+//   if (error) throw new Error(error);
+//  console.log('5656565656565');
+//   console.log(body);
+// });
+
+
+ const {data} = await axios.post('https://api.tap.company/v2/tokens', {
+   type: 'applepay',
+    token_data: { data: req.body.token.paymentData.data,
         header: 
          { ephemeralPublicKey: req.body.token.paymentData.header.ephemeralPublicKey,
            publicKeyHash: req.body.token.paymentData.header.publicKeyHash,
            transactionId: req.body.token.paymentData.header.transactionId },
         signature: req.body.token.paymentData.signature,
          version: req.body.token.paymentData.version },
-     client_ip: '192.168.1.20' },
-  json: true };
-
-request(pOptions, function (error, response, body) {
-  console.log('77777787776');
-  if (error) throw new Error(error);
- console.log('5656565656565');
-  console.log(body);
-});
-
-
- 
-
+     client_ip: '192.168.1.20'
+  }, {
+    headers: {
+      'Content-Type':'application/json',
+       authorization : 'Bearer pk_live_JC2fuQ9SNysMh4ant8ebrXUd'
+    }
+})
+  console.log({data});
 
 
   
@@ -192,7 +207,7 @@ request(pOptions, function (error, response, body) {
 // payReq.end();
     
   
-    const { data } = req.body;
+    // const { data } = req.body;
  console.log('44444');
     // console.log(payReq);
 
