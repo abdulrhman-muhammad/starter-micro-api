7723191265259eac0f7c9cef64c648296d45cccf
Abdulrhman Muhammad Abdulbadiee <eng.abdulrhman0@gmail.com>
2023-09-10 05:55:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -157,7 +157,7 @@ var request = require("request");
    { 
      accept: 'application/json',
      'content-type': 'application/json',
-     authorization: 'Bearer pk_live_JC2fuQ9SNysMh4ant8ebrXUd' },
+     authorization: 'Bearer pk_live_JC2fuQ9SNysMh4ant8ebrXU' },
   body: 
    JSON.stringify({ type: 'applepay',
      token_data: 
